f3c32f662cd8c7afbe9dfdfa4b10ebce4a52a9a8
Jayaprakash03_yes <prakash03.yes@gmail.com>
2021-04-06 15:16:00 +0530

    ccompleted sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
+import Sidebar from './Components/Sidebar/Sidebar';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<Sidebar />
 		</div>
 	);
 }
